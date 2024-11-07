6519e78b93d5272fa2ceb13f88405ef4119b6b37
Olga <kartoshkaslukom@gmail.com>
2024-11-07 19:22:56 +0100

    feat: add getusers function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './types';
 
-@Controller()
+@Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers(): User[] {
+    return this.appService.getUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './types';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private users = [
+    {
+      id: '1',
+      login: 'John Doe',
+      password: 'aaa',
+      version: 1,
+      createdAt: 1678539505000,
+      updatedAt: 1678539505111,
+    },
+    {
+      id: '2',
+      login: 'Jane Doe',
+      password: 'zzz',
+      version: 2,
+      createdAt: 1678539505111,
+      updatedAt: 1678539505111,
+    },
+  ];
+
+  getUsers(): User[] {
+    return this.users;
   }
 }
