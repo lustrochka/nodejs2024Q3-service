8f5de09e0e7e8f4f6afa1145031d7919e115e2cc
Olga <kartoshkaslukom@gmail.com>
2024-11-11 19:09:46 +0100

    feat: port is retrieved from the environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +17,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
